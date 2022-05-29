a33222a9133e6e6cfa9a982373c517db05168e08
Mithat Akbulut <marmarisco860@gmail.com>
2022-05-30 00:14:07 +0300

    Upgrade pagination component 🆙

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from "react";
 import styles from "../styles/Pagination.module.css";
 import data from "./data.json";
+import left from "../assets/menu-left.png";
+import right from "../assets/menu-right.png";
 
 function Pagination({ setSelectedPage, selectedPage }) {
   const [pages, setPages] = useState([]);
   const howManyJobsAtPage = 4;
+  //first 4 elements
+  const [paginationPart, setPaginationPart] = useState({ from: 0, to: 3 });
 
   useEffect(() => {
-    for (let i = 0; i < data.length / howManyJobsAtPage; i++) {
+    if (pages.length === 0) {
+      for (let i = 0; i < data.length / howManyJobsAtPage; i++) {
         setPages((prev) => [...prev, i]);
+      }
     }
   }, []);
 
-  useEffect(() => {
-    console.log('useeffect')
-  }, [])
+  console.log(paginationPart, pages);
 
   return (
     <div className={styles.container}>
-      {pages.map((p) => {
+      
+      {pages.slice(paginationPart.from, paginationPart.to).map((p) => {
         return (
           <PaginationElement
             id={p}
@@ -27,13 +32,27 @@ function Pagination({ setSelectedPage, selectedPage }) {
           />
         );
       })}
+
+{paginationPart.from > 0 && (
+        <LessElement
+          paginationPart={paginationPart}
+          setPaginationPart={setPaginationPart}
+          setSelectedPage={setSelectedPage}
+        />
+      )}
+
+      {paginationPart.to < pages.length && (
+        <MoreElement
+          paginationPart={paginationPart}
+          setPaginationPart={setPaginationPart}
+          setSelectedPage={setSelectedPage}
+        />
+      )}
     </div>
   );
 }
 
 function PaginationElement({ id, selectedPage, setSelectedPage }) {
-   
-    
   return (
     <button
       onClick={() => setSelectedPage(id)}
@@ -44,6 +63,40 @@ function PaginationElement({ id, selectedPage, setSelectedPage }) {
       }
     >
       {id + 1}
+    </button>
+  );
+}
+
+function MoreElement({ setPaginationPart, paginationPart, setSelectedPage }) {
+  function handleLoadMorePage() {
+    const newPart = {
+      from: paginationPart.from + 1,
+      to: paginationPart.to + 1,
+    };
+    setPaginationPart(newPart);
+    setSelectedPage((prev) => prev + 1);
+  }
+
+  return (
+    <button onClick={() => handleLoadMorePage()} className={styles.moreButton}>
+      <img src={right} width={30} alt={"right arrow"} />
+    </button>
+  );
+}
+
+function LessElement({ setPaginationPart, paginationPart, setSelectedPage }) {
+  function handleLoadLessPage() {
+    const newPart = {
+      from: paginationPart.from - 1,
+      to: paginationPart.to - 1,
+    };
+    setPaginationPart(newPart);
+    setSelectedPage((prev) => prev - 1);
+  }
+
+  return (
+    <button onClick={() => handleLoadLessPage()} className={styles.lessButton}>
+      <img src={left} width={30} alt={"left arrow"} />
     </button>
   );
 }
